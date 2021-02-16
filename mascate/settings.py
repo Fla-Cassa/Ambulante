86647ad1b7911dbb87fded3f18f8e4ad293eb626
Flavio <flacassa@gmail.com>
2021-02-16 09:30:05 -0300

    Alterado arq settings e env-sample

--- mascate/settings.py
+++ mascate/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = []
+# config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 AUTH_USER_MODEL = 'base.User'
 
